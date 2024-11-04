e592b05c91937ae61976eebd9d38e5e0d37b0b50
Jainil Ranpara <jainisoni1706@gmail.com>
2024-11-04 16:37:48 +0530

    change

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-function allowDrop(ev) {
+const allowDrop = (ev) => {
     ev.preventDefault();
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+const drag = (ev) => {
+    ev.dataTransfer.setData('text', ev.target.id);    
+}
+
+const drop = (ev) => {
+    ev.preventDefault();
+    var thePiece = ev.dataTransfer.getData('text');
+    var letterOfToBePlacedBox = ev?.target?.parentElement?.dataset.letter || ev?.target?.dataset.letter;
+    var digitOfToBePlacedBox = ev?.target?.parentElement?.dataset.digit || ev?.target?.dataset.digit;
     
+    
+
+    return false;    
+    //ev.target.appendChild(document.getElementById(data));
 }
 
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+const movement = (letter, digit, toBePlaced) => {
+    //console.log(letter, digit, toBePlaced);
+
+
 }
